7192f588deac2c49a609e9893dddcb1022eff86c
Husen <husenmalik7@gmail.com>
2025-08-12 15:00:12 +0700

    init

---
+++ twitter-like-app/src/states/isPreload/action.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+function setIsPreloadActionCreator(isPreload) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreload,
+    },
+  };
+}
+
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    try {
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      dispatch(setIsPreloadActionCreator(false));
+    }
+  };
+}
+
+export { setIsPreloadActionCreator, asyncPreloadProcess, ActionType };
