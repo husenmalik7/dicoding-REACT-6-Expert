98e87ec5b25b79dcb5b86e135c0e0cf6a36816bf
Husen <husenmalik7@gmail.com>
2025-08-12 15:51:00 +0700

    1

--- twitter-like-app/src/states/talkDetail/reducer.js
+++ twitter-like-app/src/states/talkDetail/reducer.js
@@ -1,3 +1,28 @@
-/**
- * @TODO: Define reducer for the talkDetail state
- */
+/* eslint-disable indent */
+import { ActionType } from './action';
+
+function talkDetailReducer(talkDetail = null, action = {}) {
+  switch (action.type) {
+    case ActionType.RECEIVE_TALK_DETAIL:
+      return action.payload.talks;
+
+    case ActionType.CLEAR_TALK_DETAIL:
+      return null;
+
+    case ActionType.TOGGLE_LIKE_TALK_DETAIL:
+      return action.payload.talks;
+
+    case ActionType.ADD_TALK:
+      return {
+        ...talkDetail,
+        likes: talkDetail.likes.includes(action.payload.userId)
+          ? talkDetail.likes.filter((id) => id !== action.payload.userId)
+          : talkDetail.likes.concat(action.payload.userId),
+      };
+
+    default:
+      return talkDetail;
+  }
+}
+
+export default talkDetailReducer;
