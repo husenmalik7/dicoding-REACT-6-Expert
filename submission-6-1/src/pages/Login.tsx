8356819c3a8271d3f330ca1c1bacf40874c6bcb5
Husen <husenmalik7@gmail.com>
2025-08-15 17:37:13 +0700

    1

--- submission-6-1/src/pages/Login.tsx
+++ submission-6-1/src/pages/Login.tsx
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 function Login() {
+  const navigate = useNavigate();
   return (
     <div>
       <p>Login</p>
+      <div onClick={() => navigate('/register')}>
+        <p>register</p>
+      </div>
     </div>
   );
 }
--- submission-6-1/src/pages/Register.tsx
+++ submission-6-1/src/pages/Register.tsx
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 function Register() {
+  const navigate = useNavigate();
   return (
     <div>
       <p>Register</p>
+      <div onClick={() => navigate('/login')}>
+        <p>login</p>
+      </div>
     </div>
   );
 }
