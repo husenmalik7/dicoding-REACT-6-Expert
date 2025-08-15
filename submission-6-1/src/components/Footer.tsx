564a4c2025f34edc983191ad8f95bcfb5bfbfd4d
Husen <husenmalik7@gmail.com>
2025-08-15 17:28:32 +0700

    1

--- submission-6-1/src/components/Footer.tsx
+++ submission-6-1/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import LoginIcon from '../assets/icons/LoginIcon';
 function Footer() {
   return (
     <footer>
-      <div className="fixed bottom-0 flex h-16 w-full items-center justify-center gap-4 border-1 border-gray-300 bg-white">
+      <div className="fixed bottom-0 flex h-16 w-full items-center justify-center gap-8 border-1 border-gray-300 bg-white">
         <div className="flex cursor-pointer flex-col items-center">
           <ThreadsIcon />
           <p>Threads</p>
