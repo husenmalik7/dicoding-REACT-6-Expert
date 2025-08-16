a9918357a7e28aac40c11dfc5005c1482f34cfc3
Husen <husenmalik7@gmail.com>
2025-08-16 16:00:08 +0700

    1

--- submission-6-1/src/pages/Leaderboards.tsx
+++ submission-6-1/src/pages/Leaderboards.tsx
@@ -7,6 +7,18 @@ function Leaderboards() {
         <p>Pengguna</p>
         <p>Skor</p>
       </div>
+
+      <div className="flex items-center justify-between">
+        <div className="flex h-12 items-center">
+          <img
+            className="mr-2 h-10 rounded-full"
+            src="https://ui-avatars.com/api/?name=Dimas Saputra&amp;background=random"
+            alt="Dimas"
+          />
+          <p className="text-xl">Dimassd</p>
+        </div>
+        <p className="text-xl">5555</p>
+      </div>
     </div>
   );
 }
