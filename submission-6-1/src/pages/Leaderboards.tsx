4124d03e2970b5aefa4da16c378e2a8082b061b8
Husen <husenmalik7@gmail.com>
2025-08-16 15:50:12 +0700

    1

--- submission-6-1/src/pages/Leaderboards.tsx
+++ submission-6-1/src/pages/Leaderboards.tsx
@@ -1,9 +1,9 @@
 function Leaderboards() {
   return (
     <div className="p-8">
-      <h2 className="mb-4 text-2xl">Klasemen Pengguna Aktif</h2>
+      <h2 className="mb-4 text-2xl font-medium">Klasemen Pengguna Aktif</h2>
 
-      <div className="mb-2 flex w-full justify-between bg-amber-300 text-xl">
+      <div className="mb-2 flex w-full justify-between text-xl">
         <p>Pengguna</p>
         <p>Skor</p>
       </div>
