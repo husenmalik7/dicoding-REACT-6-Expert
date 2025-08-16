91581cc56bf63656e46a3e2994ab79f639614ed3
Husen <husenmalik7@gmail.com>
2025-08-16 15:49:50 +0700

    1

--- submission-6-1/src/pages/Leaderboards.tsx
+++ submission-6-1/src/pages/Leaderboards.tsx
@@ -1,30 +1,12 @@
 function Leaderboards() {
   return (
     <div className="p-8">
-      <h2>Klasemen Pengguna Aktif</h2>
+      <h2 className="mb-4 text-2xl">Klasemen Pengguna Aktif</h2>
 
-      <p>home start</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home</p>
-      <p>home end</p>
+      <div className="mb-2 flex w-full justify-between bg-amber-300 text-xl">
+        <p>Pengguna</p>
+        <p>Skor</p>
+      </div>
     </div>
   );
 }
