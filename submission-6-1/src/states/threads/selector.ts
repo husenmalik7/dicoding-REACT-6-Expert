76683c93bc0cd19a5bb6816d3b5f78d7aed5b928
Husen <husenmalik7@gmail.com>
2025-08-18 12:37:53 +0700

    1

--- submission-6-1/src/states/threads/selector.ts
+++ submission-6-1/src/states/threads/selector.ts
@@ -3,9 +3,10 @@ import type { RootState } from '../index';
 
 const selectMappedThreads = createSelector(
   [(state: RootState) => state.threads, (state: RootState) => state.users],
-  (threads, users) => {
+  (threads, usersById) => {
     return threads.map((thread) => {
-      const owner = users.find((user) => user.id === thread.ownerId);
+      // const owner = users.find((user) => user.id === thread.ownerId);
+      const owner = usersById[thread.ownerId];
       return {
         ...thread,
         ownerName: owner ? owner.name : 'anonym',
--- submission-6-1/src/states/users/reducer.ts
+++ submission-6-1/src/states/users/reducer.ts
@@ -4,11 +4,19 @@ import type { User } from './types';
 import type { UnknownAction } from '@reduxjs/toolkit';
 
 type UsersAction = ReturnType<typeof receiveUsersActionCreator>;
+type UsersById = Record<string, User>;
 
-function usersReducer(users: User[] = [], action: UsersAction | UnknownAction) {
+function usersReducer(users: UsersById = {}, action: UsersAction | UnknownAction) {
   switch (action.type) {
-    case ActionType.RECEIVE_USERS:
-      return (action as UsersAction).payload.users;
+    case ActionType.RECEIVE_USERS: {
+      const usersArray = (action as UsersAction).payload.users;
+      const usersById = usersArray.reduce<Record<string, User>>((acc, user) => {
+        acc[user.id] = user;
+        return acc;
+      }, {});
+
+      return usersById;
+    }
 
     default:
       return users;
