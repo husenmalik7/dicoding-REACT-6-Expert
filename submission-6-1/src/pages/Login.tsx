657227de35d90c2a50c920310de72fd7c3aba2d1
Husen <husenmalik7@gmail.com>
2025-08-28 09:33:37 +0700

    1

--- submission-6-1/src/pages/Login.tsx
+++ submission-6-1/src/pages/Login.tsx
@@ -1,12 +1,22 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  const navigate = useNavigate();
   return (
-    <div>
-      <p>Login</p>
-      <div onClick={() => navigate('/register')}>
-        <p>register</p>
+    <div className="p-8">
+      <p className="text-2xl font-medium">Login</p>
+      <form className="mt-3 flex flex-col gap-2">
+        <input type="email" placeholder="email" className="rounded-lg border-1 p-2" />
+        <input type="password" placeholder="password" className="rounded-lg border-1 p-2" />
+        <button className="cursor-pointer rounded-lg bg-blue-950 p-2 text-white" type="submit">
+          Login
+        </button>
+      </form>
+
+      <div className="mt-3 flex gap-x-1">
+        Belum punya akun?
+        <Link to="/register" className="font-medium text-blue-600 visited:text-purple-600">
+          <p>Daftar di sini</p>
+        </Link>
       </div>
     </div>
   );
