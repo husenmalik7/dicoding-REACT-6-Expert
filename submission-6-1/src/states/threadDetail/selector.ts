88977ad5864bfa8441b9191f8ed85b8bbfe5e9c7
Husen <husenmalik7@gmail.com>
2025-08-28 16:34:23 +0700

    fix: selector

--- submission-6-1/src/states/threadDetail/selector.ts
+++ submission-6-1/src/states/threadDetail/selector.ts
@@ -2,19 +2,11 @@ import { createSelector } from '@reduxjs/toolkit';
 import type { RootState } from '../index';
 import { postedAt } from '../../utils';
 
-const selectMappedThreads = createSelector(
-  [(state: RootState) => state.threads, (state: RootState) => state.users],
-  (threads, usersById) => {
-    return threads.map((thread) => {
-      const owner = usersById[thread.ownerId];
-
-      return {
-        ...thread,
-        ownerName: owner ? owner.name : 'anonym',
-        postedAt: postedAt(thread.createdAt),
-      };
-    });
-  }
-);
+const selectMappedThreads = createSelector([(state: RootState) => state.threadDetail], (threadDetail) => {
+  return {
+    ...threadDetail,
+    postedAt: postedAt(threadDetail?.createdAt ?? ''),
+  };
+});
 
 export default selectMappedThreads;
