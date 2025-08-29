0bc34502744052cd2c0191b340527734f453373b
Husen <husenmalik7@gmail.com>
2025-08-29 11:38:52 +0700

    feat: login

--- submission-6-1/src/pages/Login.tsx
+++ submission-6-1/src/pages/Login.tsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
 import LoginInput from '../components/LoginInput';
+import { asyncSetAuthUser } from '../states/authUser/action';
+import { useDispatch } from 'react-redux';
+
+import type { AppDispatch } from '../states';
 
 function Login() {
+  const dispatch = useDispatch<AppDispatch>();
+
   function onLogin({ email, password }: { email: string; password: string }) {
-    console.log(email, password);
+    dispatch(asyncSetAuthUser({ email, password }));
   }
 
   return (
