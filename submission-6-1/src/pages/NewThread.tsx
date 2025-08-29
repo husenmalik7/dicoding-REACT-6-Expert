772c400bb4b79d2b81cfd0c2c467410658c0f445
Husen <husenmalik7@gmail.com>
2025-08-29 17:55:54 +0700

    123

--- submission-6-1/src/pages/NewThread.tsx
+++ submission-6-1/src/pages/NewThread.tsx
@@ -1,12 +1,42 @@
+import { useState } from 'react';
+
 function NewThread() {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [body, setBody] = useState('');
+
+  function handleAdd(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(title, category, body);
+  }
+
   return (
     <div className="p-8">
       <p className="text-2xl font-medium">Buat Diskusi Baru</p>
 
-      <form className="mt-3 flex flex-col gap-2">
-        <input type="text" placeholder="judul" className="rounded-lg border-1 p-2" />
-        <input type="text" placeholder="kategori" className="rounded-lg border-1 p-2" />
-        <textarea placeholder="body" rows={4} cols={50} className="rounded-lg border-1 p-2" />
+      <form onSubmit={(e) => handleAdd(e)} className="mt-3 flex flex-col gap-2">
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="judul"
+          className="rounded-lg border-1 p-2"
+        />
+        <input
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          type="text"
+          placeholder="kategori"
+          className="rounded-lg border-1 p-2"
+        />
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          placeholder="body"
+          rows={4}
+          cols={50}
+          className="rounded-lg border-1 p-2"
+        />
 
         <button className="cursor-pointer rounded-lg bg-blue-950 p-2 text-white" type="submit">
           Buat
