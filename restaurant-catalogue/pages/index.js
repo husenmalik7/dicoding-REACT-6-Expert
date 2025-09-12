f17b9d2d43481ed9b88d6cfc9e2b1ea8e1fef45d
Husen <husenmalik7@gmail.com>
2025-09-12 19:04:43 +0700

    1

--- restaurant-catalogue/pages/index.js
+++ restaurant-catalogue/pages/index.js
@@ -1,10 +1,25 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import RestaurantList from '../components/RestaurantsList';
 
-export default function Home() {
-    return (
-        <div className="container">
-            <p>qwe</p>
-        </div>
-    );
+export default function Home({ restaurants }) {
+  return (
+    <div className="container">
+      <RestaurantList restaurants={restaurants} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://restaurant-api.dicoding.dev/list', {
+    headers: {
+      'User-Agent': 'Next.js',
+    },
+  });
+
+  const { restaurants } = await response.json();
+
+  return {
+    props: {
+      restaurants,
+    },
+  };
 }
